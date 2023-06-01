c21402821a89dc2554c0f40a4e188f94c5e429d0
unknown <dbkaveesha@gmail.com>
2023-06-01 23:06:58 +0530

    Connect and upload my java programme in intelIJ

---
+++ src/To_check_whether_the_given_number_is_even_or_odd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class To_check_whether_the_given_number_is_even_or_odd {
+
+    public static void main(String args[]) {
+
+        System.out.println("Enter your number");
+
+        Scanner Odd = new Scanner(System.in);
+        int num = Odd.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("This is Even number");
+        } else {
+            System.out.println("This is odd number");
+        }
+
+    }
+}
