013189c6772ed700afae7a60b8269ff269d8c351
D_Binara <dbkaveesha@gmail.com>
2023-06-01 23:18:39 +0530

    Upload

---
+++ src/convert_the_temperature_in_Centigrade_to_Fahrenheit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class convert_the_temperature_in_Centigrade_to_Fahrenheit {
+
+        public static void main(String args[]){
+            System.out.println("Enter Your Value in Centigrade");
+            Scanner x = new Scanner(System.in);
+            int Cel = x.nextInt();
+
+            float Fah = Cel*9/5+32;
+
+            System.out.println("Answer is :- "+Fah);
+        }
+
+}
