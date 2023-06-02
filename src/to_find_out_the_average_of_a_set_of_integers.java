8a154cc08e420a21eaa6aa6fcdebedde155e7067
D_Binara <dbkaveesha@gmail.com>
2023-06-02 11:31:52 +0530

    update

--- src/to_find_out_the_average_of_a_set_of_integers.java
+++ src/to_find_out_the_average_of_a_set_of_integers.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class to_find_out_the_average_of_a_set_of_integers {
 
     public static void main(String[] args) {
+        int count= 0;
 
         System.out.println("Enter your number count");
 
@@ -11,16 +12,24 @@ public class to_find_out_the_average_of_a_set_of_integers {
 
         System.out.println("Enter your numbers");
 
-        int[] arr = new int[10];
+        int[] arr = new int[n];
 
         for (int i = 0; n > i; i++) {
             arr[i] = num.nextInt();
 
         }
         for (int i = 0; n > i; i++) {
-            System.out.println(arr[i]);
-
+            count = count + arr[i];
         }
+
+        System.out.println("Entered Number list are,");
+        for (int i = 0; n > i; i++) {
+            System.out.println("Number"+(i+1)+"="+arr[i]);
+        }
+
+        double average = count/n;
+        System.out.println("");
+        System.out.println("Average ="+average);
 
 
     }
